17ad968849efa8ce0981d838cf32ff55f87e6356
Andrew Blackledge <andrew@stockgamblers.com>
2024-04-10 13:23:27 -0400

    Track user login and logout in react state.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 import { firebaseConfig } from "$lib/firebase";
+import { store } from "$lib/store";
+import { login, logout } from "$lib/users";
 import { initializeApp } from "@firebase/app";
 import { GoogleAuthProvider, getAuth } from "@firebase/auth";
 import { Signin } from "@ourway/svelte-firebase-auth";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-console.log("AUTH: ", auth);
+// console.log("AUTH: ", auth);
 const googleAuthProvider = new GoogleAuthProvider();
 
+let uid: string | undefined = undefined;
 function user(e: CustomEvent) {
   console.log("CUSTOM EVENT: ", e);
+  if (e.detail.signedIn) {
+    store.dispatch(login(e.detail));
+    uid = e.detail.uid;
+  } else {
+    if (uid) {
+      store.dispatch(logout(uid));
+    } else {
+      console.error("assertion failure: uid is undefined?");
+    }
+  }
 }
 </script>
 
