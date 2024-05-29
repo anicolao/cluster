32f20aa59608b4a480916ffc8366be8dd116b749
Andrew Blackledge <andrew@stockgamblers.com>
2024-05-29 15:56:12 -0400

    Experimenting with sign in and sign out styling.
    As we try to remember different approaches to how to know what
    to show whether we're signed in or signed out, experiment with
    making the top level page display different content in each case
    and do some toying with the styling to start to create a look
    for the game.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@ import { app, firestore } from "$lib/firebase";
 import { store } from "$lib/store";
 import { login, logout } from "$lib/users";
 import { getVersion } from "$lib/version";
-import { initializeApp } from "@firebase/app";
 import { GoogleAuthProvider, getAuth } from "@firebase/auth";
 import { doc, setDoc } from "@firebase/firestore";
 import { Signin } from "@ourway/svelte-firebase-auth";
@@ -13,14 +12,18 @@ const auth = getAuth(app);
 const googleAuthProvider = new GoogleAuthProvider();
 
 let uid: string | undefined = undefined;
+let signedIn = false;
+
 function user(e: CustomEvent) {
   console.log("CUSTOM EVENT: ", e);
   if (e.detail.signedIn) {
+    signedIn = true;
     store.dispatch(login(e.detail));
     uid = e.detail.uid;
     const userRecord = doc(firestore, `/users/${uid}`);
     setDoc(userRecord, e.detail);
   } else {
+    signedIn = false;
     if (uid) {
       store.dispatch(logout(uid));
     } else {
@@ -30,7 +33,36 @@ function user(e: CustomEvent) {
 }
 </script>
 
+<div class="top">
 <h1>Cluster</h1>
-<p>Please sign in.</p>
+<p>Cluster is a space conquest game.</p>
+{#if signedIn}
+  <p>Your games are: ....</p>
+  <p>  1</p>
+  <p>  2</p>
+  <p>  3</p>
+{/if}
+<p>Cluster is an obsessive slow real-time strategy
+game in which you compete to dominate the galaxy, but in which no
+single player has the resources or power to win on their own, making diplomacy
+and cooperation key to establishing a winning alliance or a winning play.</p>
+
+{#if !signedIn}
+  <p>Please sign in.</p>
+{/if}
 <Signin {auth} {googleAuthProvider} on:user_changed={user}/>
 <p>Version {getVersion()}</p>
+</div>
+
+<style>
+  .top {
+    height: 100vh;
+    padding-left: 2em;
+    padding-right: 2em;
+    color: darkorchid;
+    background-color: black;
+  }
+  h1 {
+    margin-top: 0;
+  }
+</style>
