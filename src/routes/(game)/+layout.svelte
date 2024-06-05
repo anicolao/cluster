379a2905413b4591ee35694b008fd71c8ef56549
Andrew Blackledge <andrew@stockgamblers.com>
2024-06-05 14:27:43 -0400

    Format svelte files in VSCode.
    Use VSCode's svelte plugin to format svelte files because biome doesn't
    support svelte files yet.  We purposely didn't install this with bun in
    the hopes this gets fixed in biome soonish.
    
    Also reformat all files in the project.

--- src/routes/(game)/+layout.svelte
+++ src/routes/(game)/+layout.svelte
@@ -5,13 +5,13 @@ import { Signin } from "@ourway/svelte-firebase-auth";
 </script>
 
 <div class="top">
-    {#if $signedIn}
-        <slot/>
-    {:else}
-        <p>Please sign in.</p>
-        <Signin {auth} {googleAuthProvider} on:user_changed={user}/>
-        <div class="version">Version {getVersion()}</div>
-    {/if}
+  {#if $signedIn}
+    <slot />
+  {:else}
+    <p>Please sign in.</p>
+    <Signin {auth} {googleAuthProvider} on:user_changed={user} />
+    <div class="version">Version {getVersion()}</div>
+  {/if}
 </div>
 
 <style>
--- src/routes/(game)/lobby/+page.svelte
+++ src/routes/(game)/lobby/+page.svelte
@@ -3,20 +3,22 @@ import { auth, googleAuthProvider, user } from "$lib/auth";
 import { Signin } from "@ourway/svelte-firebase-auth";
 </script>
 
-  <div class="main">
-    <h1>Cluster</h1>
-    <p>Cluster is a space conquest game.</p>
-      <p>Your games are: ....</p>
-      <p>  1</p>
-      <p>  2</p>
-      <p>  3</p>
-    <p>Cluster is an obsessive slow real-time strategy
-    game in which you compete to dominate the galaxy, but in which no
-    single player has the resources or power to win on their own, making diplomacy
-    and cooperation key to establishing a winning alliance or a winning play.</p>
+<div class="main">
+  <h1>Cluster</h1>
+  <p>Cluster is a space conquest game.</p>
+  <p>Your games are: ....</p>
+  <p>1</p>
+  <p>2</p>
+  <p>3</p>
+  <p>
+    Cluster is an obsessive slow real-time strategy game in which you compete to
+    dominate the galaxy, but in which no single player has the resources or
+    power to win on their own, making diplomacy and cooperation key to
+    establishing a winning alliance or a winning play.
+  </p>
 
-    <Signin {auth} {googleAuthProvider} on:user_changed={user}/>
-  </div>
+  <Signin {auth} {googleAuthProvider} on:user_changed={user} />
+</div>
 
 <style>
   h1 {
